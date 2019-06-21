5c993d86a2211dce7df37d70f6e44af2700f61d0
Shynar <31266225+shynardev@users.noreply.github.com>
2019-06-21 10:03:30 -0700

    Show all blog posts on the home page.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
+
+def post_list(request):
+    posts = Post.objects.order_by('-published_date')
+    stuff_for_frontend = {'posts': posts}
+    return render(request, 'blog/post_list.html', stuff_for_frontend)
