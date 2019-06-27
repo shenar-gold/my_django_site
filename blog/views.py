da87f219a63dc10f4396af4b059506512ba7bc93
Shynar <31266225+shynardev@users.noreply.github.com>
2019-06-27 13:52:27 -0700

    Add feature to remove comments from blog posts

--- blog/views.py
+++ blog/views.py
@@ -74,3 +74,8 @@ def add_comment_to_post(request, pk):
     else:
         form = CommentForm()
     return render(request, 'blog/add_comment_to_post.html', {'form': form})
+
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.delete()
+    return redirect('post_detail', pk = comment.post.pk)
